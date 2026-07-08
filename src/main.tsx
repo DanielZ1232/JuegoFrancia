c0656ebce98b3a0348e4df0f58c0eae18e227041
Daniel Zuniga <dzuniga@ol4b.com>
2026-07-08 11:57:17 -0500

    Switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FluentProvider, webDarkTheme } from '@fluentui/react-components'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { GameProvider } from './context/GameContext'
 import App from './App'
 import './index.css'
@@ -18,9 +18,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <FluentProvider theme={customTheme} style={{ height: '100%', width: '100%', backgroundColor: 'transparent' }}>
       <GameProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </GameProvider>
     </FluentProvider>
   </React.StrictMode>,
